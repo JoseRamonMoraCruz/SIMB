bbe3b2b568992b2a5b1fff7eccb90c6884cf1231
Jose Ramon Mora Cruz <144741149+JoseRamonMoraCruz@users.noreply.github.com>
2024-05-03 09:05:38 -0600

    funcion de inicio de sesion

--- app/src/main/java/mx/tecnm/cdhidalgo/simb/Config.kt
+++ app/src/main/java/mx/tecnm/cdhidalgo/simb/Config.kt
@@ -2,7 +2,7 @@ package mx.tecnm.cdhidalgo.simb
 
 object Config {
     const val URL_BASE = "http://localhost:8080/"
-    const val PREF_NAME = "login"//si no sirve lo calo con sesion
+    const val PREF_NAME = "login"//si no sirve lo calo con "sesion"
     const val PREF_KEY_JWT = "jwt"
     const val PREF_KEY_USERNAME = "username"
 }
