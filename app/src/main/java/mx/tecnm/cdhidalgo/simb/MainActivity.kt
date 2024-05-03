35ac05848395a42cbca8e82b37ab069245f4a2dd
Jose Ramon Mora Cruz <144741149+JoseRamonMoraCruz@users.noreply.github.com>
2024-05-02 20:01:25 -0600

    creacion de la segunda pestaña

--- app/src/main/java/mx/tecnm/cdhidalgo/simb/MainActivity.kt
+++ app/src/main/java/mx/tecnm/cdhidalgo/simb/MainActivity.kt
@@ -1,11 +1,22 @@
 package mx.tecnm.cdhidalgo.simb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnCrearCuenta:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCrearCuenta = findViewById(R.id.btnNewCountLogin)
+
+        btnCrearCuenta.setOnClickListener {
+            val intent = Intent(this, crearcuentaInterfaz::class.java)
+            startActivity(intent)
+        }
     }
 }
