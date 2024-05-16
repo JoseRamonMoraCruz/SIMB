2256314a63ce33ff088427920233bdbd25979f62
Jose Ramon Mora Cruz <144741149+JoseRamonMoraCruz@users.noreply.github.com>
2024-05-15 18:58:16 -0600

    app 15/05/2024

--- app/src/main/java/mx/tecnm/cdhidalgo/simb/Cuenta.kt
+++ app/src/main/java/mx/tecnm/cdhidalgo/simb/Cuenta.kt
@@ -1,11 +1,20 @@
 package mx.tecnm.cdhidalgo.simb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Cuenta : AppCompatActivity() {
+    private lateinit var editarbtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cuenta)
+
+        editarbtn = findViewById(R.id.tveditarCuentaEdit)
+
+        editarbtn.setOnClickListener {
+            startActivity(Intent(this, EditCuenta::class.java))
+        }
     }
 }
