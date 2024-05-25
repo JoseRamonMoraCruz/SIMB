7acdd2aec5262fd61a27324f64b45add4f1e536a
Kirby792 <142421392+Kirby792@users.noreply.github.com>
2024-05-25 14:00:01 -0600

    listo

---
+++ app/src/main/java/mx/tecnm/cdhidalgo/simb/DeteccionMovActivity.kt
@@ -0,0 +1,12 @@
+package mx.tecnm.cdhidalgo.simb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeteccionMovActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deteccion_mov)
+    }
+
+}
---
+++ app/src/main/java/mx/tecnm/cdhidalgo/simb/ajuste_gas.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.cdhidalgo.simb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ajuste_gas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ajuste_gas)
+    }
+}
